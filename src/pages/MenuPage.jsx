c495e60f8a0770bf8870140a9427cdbf88726729
mazali01 <mazal.iliad@gmail.com>
2025-09-27 21:08:08 +0300

    feat: Update MenuPage to display a placeholder message for the wedding menu

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -5,7 +5,7 @@ const MenuPage = () => {
     return (
         <PageTemplate title="מתלבטים מה לאכול? 🍽️">
             <p style={{ color: '#2c2c2c', fontSize: '1.2rem' }}>
-                כאן תוכלו לראות את התפריט המלא של החתונה עם כל המנות הטעימות!
+                בקרוב...
             </p>
         </PageTemplate>
     );
